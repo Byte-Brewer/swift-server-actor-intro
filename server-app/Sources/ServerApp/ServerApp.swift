79df29c4f6f826596f4600d2b925bc7fa74a13d9
limpid-kzonix <balyszyn@gmail.com>
2022-12-25 18:19:47 +0000

    update something

--- server-app/Sources/ServerApp/ServerApp.swift
+++ server-app/Sources/ServerApp/ServerApp.swift
@@ -4,14 +4,12 @@ import Foundation
 import NIO
 import NIOHTTP1
 
-struct Greeting: Content {
-    var hello: String
-}
-
 actor ServerUptime {
-    private var startedAt = Date()
+    private var startedAt: Date = Date()
+
     func uptime() -> String {
-        let diffComponents = Calendar.current.dateComponents([.hour, .minute, .second], from: startedAt, to: Date())
+        let diffComponents = Calendar.current
+            .dateComponents([.hour, .minute, .second], from: startedAt, to: Date())
         let hours = diffComponents.hour ?? 0
         let minutes = diffComponents.minute ?? 0
         let seconds = diffComponents.second ?? 0
@@ -19,19 +17,23 @@ actor ServerUptime {
     }
 }
 
-let uptime: ServerUptime = ServerUptime()
-
 public func routes(instance app: Application) throws {
+    let uptime: ServerUptime = ServerUptime()
+
     app.get("main") { req async throws -> View in 
         let interval: String = await uptime.uptime()
-        let addr: SocketAddress? = req.remoteAddress 
+        let addr: SocketAddress? = req.remoteAddress
         let ip: String = addr?.ipAddress ?? "unknown"
-        return try await req.view.render("main", ["name": ip, "surname": "\(addr)", "time": "\(interval)"])
+        return try await req.view.render("main", [
+                "ip": ip,
+                "name": "username",
+                "time": "\(interval)"
+            ]
+        )
     }
 }
 
 public func configure(instance app: Application) throws {
     app.views.use(.leaf)
-
     try routes(instance: app)
 }
